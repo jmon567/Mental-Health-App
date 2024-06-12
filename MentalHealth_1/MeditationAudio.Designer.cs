956795695b603bbd1af911c82a9b5c69de767dc4
John Miller <jmon4467@outlook.com>
2024-06-12 17:58:10 -0500

    june 12 - 6pm failed audio experiment

--- MentalHealth_1/MeditationAudio.Designer.cs
+++ MentalHealth_1/MeditationAudio.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MeditationAudio));
             label1 = new Label();
             pictureBox_174 = new PictureBox();
@@ -41,6 +42,16 @@
             pictureBox_741 = new PictureBox();
             button_back = new Button();
             button_stopaudio = new Button();
+            panel_174 = new Panel();
+            panel_417 = new Panel();
+            panel_639 = new Panel();
+            panel_396 = new Panel();
+            panel_852 = new Panel();
+            panel_528 = new Panel();
+            panel_285 = new Panel();
+            panel_741 = new Panel();
+            panel_963 = new Panel();
+            errorProvider1 = new ErrorProvider(components);
             ((System.ComponentModel.ISupportInitialize)pictureBox_174).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox_285).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox_396).BeginInit();
@@ -50,6 +61,16 @@
             ((System.ComponentModel.ISupportInitialize)pictureBox_639).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox_417).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox_741).BeginInit();
+            panel_174.SuspendLayout();
+            panel_417.SuspendLayout();
+            panel_639.SuspendLayout();
+            panel_396.SuspendLayout();
+            panel_852.SuspendLayout();
+            panel_528.SuspendLayout();
+            panel_285.SuspendLayout();
+            panel_741.SuspendLayout();
+            panel_963.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).BeginInit();
             SuspendLayout();
             // 
             // label1
@@ -64,19 +85,20 @@
             // pictureBox_174
             // 
             pictureBox_174.Image = Properties.Resources._174Hz;
-            pictureBox_174.Location = new Point(71, 170);
+            pictureBox_174.Location = new Point(15, 19);
             pictureBox_174.Name = "pictureBox_174";
-            pictureBox_174.Size = new Size(200, 100);
+            pictureBox_174.Size = new Size(119, 100);
             pictureBox_174.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox_174.TabIndex = 1;
             pictureBox_174.TabStop = false;
+            pictureBox_174.Click += pictureBox_174_Click;
             // 
             // pictureBox_285
             // 
             pictureBox_285.Image = Properties.Resources._285Hz;
-            pictureBox_285.Location = new Point(367, 170);
+            pictureBox_285.Location = new Point(18, 25);
             pictureBox_285.Name = "pictureBox_285";
-            pictureBox_285.Size = new Size(200, 100);
+            pictureBox_285.Size = new Size(119, 100);
             pictureBox_285.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox_285.TabIndex = 2;
             pictureBox_285.TabStop = false;
@@ -84,9 +106,9 @@
             // pictureBox_396
             // 
             pictureBox_396.Image = Properties.Resources._369HZ;
-            pictureBox_396.Location = new Point(721, 170);
+            pictureBox_396.Location = new Point(19, 25);
             pictureBox_396.Name = "pictureBox_396";
-            pictureBox_396.Size = new Size(200, 100);
+            pictureBox_396.Size = new Size(114, 100);
             pictureBox_396.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox_396.TabIndex = 3;
             pictureBox_396.TabStop = false;
@@ -94,9 +116,9 @@
             // pictureBox_528
             // 
             pictureBox_528.Image = Properties.Resources._528Hz;
-            pictureBox_528.Location = new Point(367, 315);
+            pictureBox_528.Location = new Point(21, 27);
             pictureBox_528.Name = "pictureBox_528";
-            pictureBox_528.Size = new Size(200, 100);
+            pictureBox_528.Size = new Size(119, 100);
             pictureBox_528.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox_528.TabIndex = 4;
             pictureBox_528.TabStop = false;
@@ -104,9 +126,9 @@
             // pictureBox_852
             // 
             pictureBox_852.Image = Properties.Resources._852Hz;
-            pictureBox_852.Location = new Point(367, 474);
+            pictureBox_852.Location = new Point(21, 28);
             pictureBox_852.Name = "pictureBox_852";
-            pictureBox_852.Size = new Size(200, 100);
+            pictureBox_852.Size = new Size(112, 100);
             pictureBox_852.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox_852.TabIndex = 5;
             pictureBox_852.TabStop = false;
@@ -114,9 +136,9 @@
             // pictureBox_963
             // 
             pictureBox_963.Image = Properties.Resources._963Hz;
-            pictureBox_963.Location = new Point(721, 474);
+            pictureBox_963.Location = new Point(19, 32);
             pictureBox_963.Name = "pictureBox_963";
-            pictureBox_963.Size = new Size(200, 100);
+            pictureBox_963.Size = new Size(116, 100);
             pictureBox_963.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox_963.TabIndex = 6;
             pictureBox_963.TabStop = false;
@@ -124,9 +146,9 @@
             // pictureBox_639
             // 
             pictureBox_639.Image = Properties.Resources._639Hz;
-            pictureBox_639.Location = new Point(721, 315);
+            pictureBox_639.Location = new Point(19, 27);
             pictureBox_639.Name = "pictureBox_639";
-            pictureBox_639.Size = new Size(200, 100);
+            pictureBox_639.Size = new Size(116, 100);
             pictureBox_639.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox_639.TabIndex = 7;
             pictureBox_639.TabStop = false;
@@ -134,9 +156,9 @@
             // pictureBox_417
             // 
             pictureBox_417.Image = Properties.Resources._417Hz;
-            pictureBox_417.Location = new Point(71, 315);
+            pictureBox_417.Location = new Point(15, 27);
             pictureBox_417.Name = "pictureBox_417";
-            pictureBox_417.Size = new Size(200, 100);
+            pictureBox_417.Size = new Size(119, 100);
             pictureBox_417.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox_417.TabIndex = 8;
             pictureBox_417.TabStop = false;
@@ -144,16 +166,16 @@
             // pictureBox_741
             // 
             pictureBox_741.Image = Properties.Resources._741Hz;
-            pictureBox_741.Location = new Point(71, 474);
+            pictureBox_741.Location = new Point(15, 31);
             pictureBox_741.Name = "pictureBox_741";
-            pictureBox_741.Size = new Size(200, 100);
+            pictureBox_741.Size = new Size(119, 100);
             pictureBox_741.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox_741.TabIndex = 9;
             pictureBox_741.TabStop = false;
             // 
             // button_back
             // 
-            button_back.Location = new Point(968, 545);
+            button_back.Location = new Point(1219, 732);
             button_back.Name = "button_back";
             button_back.Size = new Size(150, 46);
             button_back.TabIndex = 10;
@@ -163,7 +185,7 @@
             // 
             // button_stopaudio
             // 
-            button_stopaudio.Location = new Point(978, 315);
+            button_stopaudio.Location = new Point(1219, 170);
             button_stopaudio.Name = "button_stopaudio";
             button_stopaudio.Size = new Size(150, 46);
             button_stopaudio.TabIndex = 11;
@@ -171,22 +193,98 @@
             button_stopaudio.UseVisualStyleBackColor = true;
             button_stopaudio.Click += button_stopaudio_Click;
             // 
+            // panel_174
+            // 
+            panel_174.Controls.Add(pictureBox_174);
+            panel_174.Location = new Point(92, 161);
+            panel_174.Name = "panel_174";
+            panel_174.Size = new Size(148, 148);
+            panel_174.TabIndex = 12;
+            // 
+            // panel_417
+            // 
+            panel_417.Controls.Add(pictureBox_417);
+            panel_417.Location = new Point(92, 377);
+            panel_417.Name = "panel_417";
+            panel_417.Size = new Size(159, 154);
+            panel_417.TabIndex = 2;
+            // 
+            // panel_639
+            // 
+            panel_639.Controls.Add(pictureBox_639);
+            panel_639.Location = new Point(871, 389);
+            panel_639.Name = "panel_639";
+            panel_639.Size = new Size(159, 154);
+            panel_639.TabIndex = 13;
+            // 
+            // panel_396
+            // 
+            panel_396.Controls.Add(pictureBox_396);
+            panel_396.Location = new Point(871, 170);
+            panel_396.Name = "panel_396";
+            panel_396.Size = new Size(159, 154);
+            panel_396.TabIndex = 3;
+            // 
+            // panel_852
+            // 
+            panel_852.Controls.Add(pictureBox_852);
+            panel_852.Location = new Point(474, 624);
+            panel_852.Name = "panel_852";
+            panel_852.Size = new Size(159, 154);
+            panel_852.TabIndex = 3;
+            // 
+            // panel_528
+            // 
+            panel_528.Controls.Add(pictureBox_528);
+            panel_528.Location = new Point(474, 377);
+            panel_528.Name = "panel_528";
+            panel_528.Size = new Size(159, 154);
+            panel_528.TabIndex = 3;
+            // 
+            // panel_285
+            // 
+            panel_285.Controls.Add(pictureBox_285);
+            panel_285.Location = new Point(467, 161);
+            panel_285.Name = "panel_285";
+            panel_285.Size = new Size(159, 154);
+            panel_285.TabIndex = 3;
+            // 
+            // panel_741
+            // 
+            panel_741.Controls.Add(pictureBox_741);
+            panel_741.Location = new Point(92, 609);
+            panel_741.Name = "panel_741";
+            panel_741.Size = new Size(159, 154);
+            panel_741.TabIndex = 3;
+            // 
+            // panel_963
+            // 
+            panel_963.Controls.Add(pictureBox_963);
+            panel_963.Location = new Point(871, 631);
+            panel_963.Name = "panel_963";
+            panel_963.Size = new Size(159, 154);
+            panel_963.TabIndex = 3;
+            // 
+            // errorProvider1
+            // 
+            errorProvider1.ContainerControl = this;
+            // 
             // MeditationAudio
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1199, 685);
+            ClientSize = new Size(1437, 859);
+            Controls.Add(panel_963);
+            Controls.Add(panel_396);
+            Controls.Add(panel_852);
+            Controls.Add(panel_528);
+            Controls.Add(panel_285);
+            Controls.Add(panel_741);
+            Controls.Add(panel_639);
+            Controls.Add(panel_417);
+            Controls.Add(panel_174);
             Controls.Add(button_stopaudio);
             Controls.Add(button_back);
-            Controls.Add(pictureBox_741);
-            Controls.Add(pictureBox_417);
-            Controls.Add(pictureBox_639);
-            Controls.Add(pictureBox_963);
-            Controls.Add(pictureBox_852);
-            Controls.Add(pictureBox_528);
-            Controls.Add(pictureBox_396);
-            Controls.Add(pictureBox_285);
-            Controls.Add(pictureBox_174);
             Controls.Add(label1);
             Name = "MeditationAudio";
             Text = "Mediation Audio";
@@ -199,6 +297,16 @@
             ((System.ComponentModel.ISupportInitialize)pictureBox_639).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox_417).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox_741).EndInit();
+            panel_174.ResumeLayout(false);
+            panel_417.ResumeLayout(false);
+            panel_639.ResumeLayout(false);
+            panel_396.ResumeLayout(false);
+            panel_852.ResumeLayout(false);
+            panel_528.ResumeLayout(false);
+            panel_285.ResumeLayout(false);
+            panel_741.ResumeLayout(false);
+            panel_963.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).EndInit();
             ResumeLayout(false);
         }
 
@@ -216,5 +324,15 @@
         private PictureBox pictureBox_741;
         private Button button_back;
         private Button button_stopaudio;
+        private Panel panel_174;
+        private Panel panel_417;
+        private Panel panel_639;
+        private Panel panel_396;
+        private Panel panel_852;
+        private Panel panel_528;
+        private Panel panel_285;
+        private Panel panel_741;
+        private Panel panel_963;
+        private ErrorProvider errorProvider1;
     }
 }
--- MentalHealth_1/MeditationAudio.cs
+++ MentalHealth_1/MeditationAudio.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
 namespace MentalHealth_1
 {
@@ -24,7 +25,8 @@ namespace MentalHealth_1
             bool Hz741 = false;
             bool Hz852 = false;
             bool Hz963 = false;
-
+          
+            
         }
 
         private void button_back_Click(object sender, EventArgs e)
@@ -44,6 +46,30 @@ namespace MentalHealth_1
             bool Hz741 = false;
             bool Hz852 = false;
             bool Hz963 = false;
+        }
+
+        private void pictureBox_174_Click(object sender, EventArgs e)
+        {
+            bool Hz174 = true;
+            while (Hz174)
+            {
+                try
+                {
+                    SoundPlayer Audio174 = new SoundPlayer();
+                    Audio174.SoundLocation = @"C:iClould Drive\Desktop\**MS539Code_VS\MentalHealthApp\MentalHealth_1\MentalHealth_1\bin\Debug\net8.0-windows\174Hz.m4a";
+                    Audio174.Play();
+
+                }
+                catch
+                {
+                    errorProvider1.SetError(pictureBox_174, "Audio Not Found");
+                    //Console.WriteLine("Audio Not Found");
+                }
+                
+            }
+
+
+
         }
     }
 }
